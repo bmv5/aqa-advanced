46bdba13b5bcae6eb0dfceb47cceb8e0d48304b4
Bobkova <maryna.bobkova@sigma.software>
2024-12-30 10:47:21 +0100

    9.1 Objects in Java Script

---
+++ homeWork/hw9/task5.js
@@ -0,0 +1,11 @@
+// Створення масиву об'єктів users
+const users = [
+    { name: "Alice", email: "alice@example.com", age: 25, gender: "female" },
+    { name: "Bob", email: "bob@example.com", age: 30, gender: "male" },
+    { name: "Charlie", email: "charlie@example.com", age: 35, gender: "male" }
+];
+
+// Перебір масиву за допомогою циклу for...of з деструктуризацією
+for (const { name, email, age, gender } of users) {
+    console.log(`Name: ${name}, Email: ${email}, Age: ${age}, Gender: ${gender}`);
+}
